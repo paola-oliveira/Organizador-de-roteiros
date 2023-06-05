c90ecb30c4ea7818d9c1e89a5524a8d7a5fa9e9b
paola-oliveira <paolanatalia_oliveira@outlook.com>
2023-06-05 09:41:08 -0300

    finalização do projeto

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,10 +1,15 @@
+import Colaborador from '../Colaborador'
 import './Time.css'
 
 const Time = (props) => {
     return (
-        <section className='time' style={{backgroundColor: props.corSecundaria}}>
+        (props.viagem.length > 0) ? <section className='time' style={{backgroundColor: props.corSecundaria}}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.viagem.map(viagem => <Colaborador nome={viagem.nome} cargo={viagem.cargo} imagem={viagem.imagem}/>)}
+            </div>
         </section>
+        : ''
     )
 }
 
